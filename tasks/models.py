a7e9b7a24ab8022439e2a6eab07de711ee7710ab
Mahsa R <mahsarm99@gmail.com>
2023-07-31 16:05:00 +0330

    add CardModel to tasks/models.py

--- tasks/models.py
+++ tasks/models.py
@@ -20,6 +20,29 @@ class ListModel(BaseModel):
     background_color = models.CharField(
         max_length=10,choices=COLOR_CHOICES, null=True, blank=True)
     
+
+class CardModel(BaseModel):
+    title = models.CharField(verbose_name=_("Title"),max_length=150,
+                            help_text=_("Enter Card title"))
+    description = models.TextField(verbose_name=_("Descripion"),
+                                   help_text=_("Enter card's describtion"),null=True,blank=True)
     
+    start_date = models.DateTimeField(verbose_name=_("Start Time"),auto_now=True)
+    due_date = models.DateTimeField(verbose_name=_("Due Time"),auto_now=True)
+    reminder_time = models.DateTimeField(verbose_name=_("Reminder Time"),auto_now=True)
+    has_reminder = models.BooleanField(default=False)
+    
+    list = models.ForeignKey(ListModel, on_delete=models.CASCADE, related_name='cards')
+    
+    STATUS_CHOICES = [
+    ('todo', 'Todo'),
+    ('doing','Doing'),
+    ('done', 'Done'),
+    ('suspended','Suspended'),
+    ]
+    status = models.CharField(
+        max_length=20,choices=STATUS_CHOICES, default='todo')
+    
+    background_img = models.ImageField(upload_to='tasks', null=True, blank=True)
     
     
