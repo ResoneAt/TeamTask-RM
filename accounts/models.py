84ef26bd6173f909d3344a72c61291996b14bf7e
Ramin <eslamiramin85@gmail.com>
2023-07-31 23:20:45 +0330

    add some methods to User model in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-
+from django.shortcuts import reverse
 from core.models import BaseModel
 from .manager import MyUserManager
 from django.utils.translation import gettext_lazy as _
@@ -72,9 +72,6 @@ class User(AbstractBaseUser):
         verbose_name, verbose_name_plural = _('User'), _('Users')
         db_table = 'User'
 
-    def __str__(self):
-        return self.email
-
     def has_perm(self, perm, obj=None):
         return True
 
@@ -85,18 +82,51 @@ class User(AbstractBaseUser):
     def is_staff(self):
         return self.is_admin
 
+    def seen_pv_message(self, from_user):
+        messages = PvMessageModel.objects.filter(from_user=from_user,
+                                                 to_user=self,
+                                                 is_read=False)
+        messages.update(is_read=True)
+
+    def add_to_work_space(self):
+        ...
+
+    def add_to_board(self):
+        ...
+
+    def add_to_card(self):
+        ...
+
+    def delete_from_work_space(self):
+        ...
+
+    def delete_from_board(self):
+        ...
+
+    def delete_from_card(self):
+        ...
+
+    def get_absolut_url(self):
+        kwargs = {
+            'user_id': self.pk
+        }
+        return reverse('accounts:user_profile', kwargs=kwargs)
+
+    def __str__(self):
+        return self.email
 
 
-class GmessageModel(BaseModel):
+class GMessageModel(BaseModel):
     from_user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='sender')
     text = models.TextField(help_text='Please Write Your Message')
-    board = models.ForeignKey(BoardModel , on_delete=models.CASCADE)
+    board = models.ForeignKey('BoardModel' , on_delete=models.CASCADE)
     
     class Meta:
         verbose_name, verbose_name_plural = _("GMessage"), _("GMessages")
         db_table = 'Gmessage'
 
-class PvmessageModel(BaseModel):
+
+class PvMessageModel(BaseModel):
     from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
     to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
     text = models.TextField(help_text='Please Write Your Message')
@@ -106,10 +136,11 @@ class PvmessageModel(BaseModel):
         verbose_name, verbose_name_plural = _("Message"), _("Messages")
         db_table = 'Pvmessage'
 
+
 class NotificationModel(BaseModel):
     body = models.TextField(null=True, blank=True)
     to_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    card = models.ForeignKey(CardModel, on_delete=models.CASCADE)
+    card = models.ForeignKey('CardModel', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name, verbose_name_plural = _("Notification"), _("Notifications")
