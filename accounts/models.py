ad61a09d5a15f4c65f8acfb36341b90b8136c80a
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-07-31 17:27:29 +0330

    create NotificationModel in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -106,12 +106,14 @@ class PvmessageModel(BaseModel):
         verbose_name, verbose_name_plural = _("Message"), _("Messages")
         db_table = 'Pvmessage'
 
+class NotificationModel(BaseModel):
+    body = models.TextField(null=True, blank=True)
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card = models.ForeignKey(CardModel, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name, verbose_name_plural = _("Notification"), _("Notifications")
+
     
-
-
-
-
-
-
 
 
