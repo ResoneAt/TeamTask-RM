e6c914391a0621af0a49a6704f3120231e4aea60
Ramin <eslamiramin85@gmail.com>
2023-08-01 22:03:25 +0330

    create SoftDelete model and modify BaseModel in core/models.py

--- core/models.py
+++ core/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(verbose_name=_('created at'),
+                                      auto_now_add=True)
 
     class Meta:
         abstract = True
         ordering = ['-created_at']
+
+
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(verbose_name=_('Is Deleted'),
+                                     default=False)
+    deleted_at = models.DateTimeField(verbose_name=_('Deleted at'),
+                                      null=True,
+                                      blank=True,
+                                      editable=False)
+
+    class Meta:
+        abstract = True
