1665b685ce3f1a877d619f4233612e87ccc5c20e
Ramin <eslamiramin85@gmail.com>
2023-08-02 23:07:29 +0330

    move GMessageModel to tasks/models.py

--- TeamTask/settings.py
+++ TeamTask/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'accounts.apps.AccountsConfig',
+    'tasks.apps.TasksConfig',
+    'core.apps.CoreConfig',
+
 ]
 
 MIDDLEWARE = [
