1f510e7b8abfaeb988960d46caca57264fa88341
Ramin <eslamiramin85@gmail.com>
2023-08-03 22:19:11 +0330

    update requirements.txt

--- TeamTask/settings.py
+++ TeamTask/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'TeamTask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TeamTask-db',
+        'USER': 'Ramin',
+        'PASSWORD': 'resoneesami',
+        'PORT': '5432',
+        'HOST': '127.0.0.1'
     }
 }
 
