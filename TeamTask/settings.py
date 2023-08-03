52e438dc13917eab327bac3d925a9818509f2273
Ramin <eslamiramin85@gmail.com>
2023-08-03 22:24:39 +0330

    modify settings.py

--- TeamTask/settings.py
+++ TeamTask/settings.py
@@ -79,17 +79,23 @@ WSGI_APPLICATION = 'TeamTask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'TeamTask-db',
+#         'USER': 'Ramin',
+#         'PASSWORD': 'resoneesami',
+#         'PORT': '5432',
+#         'HOST': '127.0.0.1'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TeamTask-db',
-        'USER': 'Ramin',
-        'PASSWORD': 'resoneesami',
-        'PORT': '5432',
-        'HOST': '127.0.0.1'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
