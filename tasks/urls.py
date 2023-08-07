71603be72bced667a12173af23a819b13bbc6f7e
Mahsa R <mahsarm99@gmail.com>
2023-08-07 18:12:01 +0330

    add showmycards view in tasks/views.py and tasks/urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 
 
+app_name = 'tasks'
 urlpatterns = [
+	path('mycards/', views.ShowMyCards.as_view(), name='show_my_cards'),
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from .models import CardModel
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+class ShowMyCards(LoginRequiredMixin,View):
+    template_name = 'show_my_cards.html'
+    
+    def get(self, request, *args, **kwargs):
+        cards = CardModel.objects.filter(user=request.user) 
+        return render(request, self.template_name, context={'cards': cards})
