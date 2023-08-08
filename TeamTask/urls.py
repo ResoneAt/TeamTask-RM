962b9c838418fa169fc98381b54c82d064a87840
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-08 17:59:32 +0330

    create navbar in templates/include/navbar.html and create url for home and accounts

--- TeamTask/urls.py
+++ TeamTask/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls', namespace='home')),
+    path('account/', include ('account.urls', namespace='account')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
