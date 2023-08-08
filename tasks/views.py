c7d9056ca4773e71bd3246c6a0f5ae40417ba8d5
Mahsa R <mahsarm99@gmail.com>
2023-08-08 13:17:31 +0330

    edit showmycards view in tasks/views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from .models import CardModel
+from .models import CardModel, ListModel
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
@@ -8,5 +8,12 @@ class ShowMyCards(LoginRequiredMixin,View):
     template_name = 'show_my_cards.html'
     
     def get(self, request, *args, **kwargs):
-        cards = CardModel.objects.filter(user=request.user) 
-        return render(request, self.template_name, context={'cards': cards})
+        # cards = CardModel.objects.filter(user=request.user) 
+        # return render(request, self.template_name, context={'cards': cards})
+        lists = ListModel.objects.filter(user=request.user).prefetch_related('cards')
+        return render(request, self.template_name, {'lists': lists})
+        
+        
+
+    
+
