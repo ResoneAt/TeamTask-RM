946981dfe61e85fa31872a399642638c9dca97bd
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-08 18:29:20 +0330

    create url logout in accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 urlpatterns =[
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
      path('login/', views.UserLoginView.as_view(), name='user_login'),
+     path('logout/', views.UserLogoutView.as_view(), name= 'user_logout'),
 ]
