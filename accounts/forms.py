d141776e1c74a8c0e92d0e07500652f2e1b1e76e
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-08 18:36:42 +0330

    create vlidation for UserRegistrationForm in accounts/forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,6 +11,21 @@ class UserRegistrationForm(forms.Form):
     password1 = forms.CharField(label='password',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder': 'your password'}))
     password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class':'form-control','placeholder': 'your password'}))
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('this Email already exists')
+        return email
+    
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and p1 != p2:
+            raise ValidationError ('password must match')
+        
 
 class UserLoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'username'}))
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns =[
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
-     path('login/', views.UserLoginView.as_view(), name='user_login'),
-     path('logout/', views.UserLogoutView.as_view(), name= 'user_logout'),
+    path('login/', views.UserLoginView.as_view(), name='user_login'),
+    path('logout/', views.UserLogoutView.as_view(), name= 'user_logout'),
 ]
