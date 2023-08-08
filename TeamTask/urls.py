fc38bf886ed001ad1c3079b398dd0882ed87cacd
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-08 21:12:50 +0330

    edit TeamTask/urls.py

--- TeamTask/urls.py
+++ TeamTask/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls', namespace='home')),
-    path('account/', include ('accounts.urls', namespace='account')),
+    path('accounts/', include ('accounts.urls', namespace='account')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
