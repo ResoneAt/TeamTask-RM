e789d598a15b63f7e1d7e7642eb1863968cc937e
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-08 18:20:07 +0330

    create url login and create login in navbar for accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns =[
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
+     path('login/', views.UserLoginView.as_view(), name='user_login'),
 ]
