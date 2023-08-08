b5118897649f5d5910bf1e89d2e3d650bbd34289
Mahsa R <mahsarm99@gmail.com>
2023-08-08 13:48:47 +0330

    edit tasks/urls.py for cardseditview

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'tasks'
 urlpatterns = [
 	path('mycards/', views.ShowMyCards.as_view(), name='show_my_cards'),
+    path('edit_card/<int:card_id>/', views.CardEditView.as_view(), name='edit_card'),
+
 
 ]
