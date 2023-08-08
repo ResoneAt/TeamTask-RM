81f33da1122d299740bafed0d3c2316928783a89
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-08-08 18:03:50 +0330

    add url for createboardview in tasks/urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('edit_card/<int:card_id>/', views.CardEditView.as_view(), name='edit_card'),
     path('workspace/create/',views.CreateWorkSpaceView.as_view(),name='create_workspace'),
     path('workspace/<int:workspace_id>/edit/',views.EditWorkSpaceView.as_view(),name='edit_workspace'),
-    
+    path('workspace/<int:workspace_id>/board/create/',views.CreateBoardView.as_view(),name='create_board'),
 
 ]
