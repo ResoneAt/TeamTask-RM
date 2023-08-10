c0249ed69c9f171cec88b531e252e5af406024cf
Ramin <eslamiramin85@gmail.com>
2023-08-10 11:24:04 +0330

    add login() to UserRegisterView in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -20,18 +20,20 @@ class UserRegisterView(View):
 
     def get(self, request):
         form = self.form_class()
-        return render(request, self.template_name, {'form':form})
+        return render(request, self.template_name, {'form': form})
     
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd['username'],
-                                    cd['email'],
-                                    cd['password1'])
-            messages.success(request, 'you registred successfully', 'success')
+            user = User.objects.create_user(cd['username'],
+                                            cd['email'],
+                                            cd['password1'])
+            login(request, user)
+            messages.success(request, 'you registered successfully', 'success')
             return redirect('home:home')
-        return render(request, self.template_name, {'form':form})
+        return render(request, self.template_name, {'form': form})
+
 
 class UserLoginView(View):
     from_class = UserLoginForm
