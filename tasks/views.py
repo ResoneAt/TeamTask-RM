55468e4af42aae5edcb5fac85db8d2976f8a6271
Mahsa R <mahsarm99@gmail.com>
2023-08-14 11:52:49 +0330

    add SubCardCreate in tasks/views.py

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import CardModel, WorkSpaceModel, BoardModel, ListModel
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CardCreateEditForm,LabelCreateEditForm, WorkSpaceForm, BoardForm
+from .forms import CardCreateEditForm,LabelCreateEditForm,SubCardCreateEditForm,\
+    WorkSpaceForm, BoardForm
 
 
 class MyCardsView(LoginRequiredMixin, View):
@@ -252,6 +253,27 @@ class LabelDeleteView(LoginRequiredMixin, View):
 
 class SubCardCreateView(LoginRequiredMixin, View):
     template_name = 'tasks/sub_card_create.html'
+    card_instance : object
+    form_class = SubCardCreateEditForm
+
+    def setup(self, request, *args, **kwargs):
+        self.card_instance = get_object_or_404(CardModel,
+                                                pk=kwargs['card_id'])
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_subcard = form.save(commit=False)
+            new_subcard.card = self.card_instance
+            new_subcard.save()
+            messages.success(request, 'you created a new subcard', 'success')
+            return redirect(self.card_instance.get_absolute_url())
+        return render(request, self.template_name, {'form': form})
 
 
 class SubCardEditView(LoginRequiredMixin, View):
