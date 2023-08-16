eb750df045d814687f63a4663fbc13547e12f375
Ramin <eslamiramin85@gmail.com>
2023-08-16 19:01:33 +0330

    rename core project Dir from TeamTask to project

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'TeamTask.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'TeamTask.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
