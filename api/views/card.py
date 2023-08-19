76f0eb87dba23b3609e091eeae0267c35e1174de
Mahsa R <mahsarm99@gmail.com>
2023-08-19 18:24:13 +0330

    add ListViewSet in api/views/card.py

--- api/views/card.py
+++ api/views/card.py
@@ -1,5 +1,9 @@
 from rest_framework.viewsets import ViewSet
-
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from tasks.models import ListModel,BoardModel
+from api.serializers.card import ListSerializer
+from rest_framework import status
 
 class MyCardViewSet(ViewSet):
     def list(self, request):
@@ -42,15 +46,28 @@ class SubCardViewSet(ViewSet):
 
 
 class ListViewSet(ViewSet):
-
-    def create(self, request):
-        ...
+    
+    def create(self, request, id): 
+        board_instance = get_object_or_404(BoardModel, id=id)
+        srz_data = ListSerializer(data=request.data)
+        if srz_data.is_valid():
+            srz_data.object.board = board_instance
+            srz_data.save()
+            return Response(srz_data.data, status=status.HTTP_201_CREATED)
+        return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def partial_update(self, request, pk=None):
-        ...
+        list = ListModel.objects.get(pk=pk)
+        srz_data =ListSerializer(instance=list, data=request.data, partial=True)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response(srz_data.data, status=status.HTTP_200_OK)
+        return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def destroy(self, request):
-        ...
+    def destroy(self, request, pk):
+        list = ListModel.objects.get(pk=pk)
+        list.delete()
+        return Response({'message': 'list deleted'}, status=status.HTTP_200_OK)
 
 
 class LabelViewSet(ViewSet):
