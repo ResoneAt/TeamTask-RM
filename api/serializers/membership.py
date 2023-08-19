2c5113a1e9ecd3e55e2fda5c5d6399e6f551fc3a
Ramin <eslamiramin85@gmail.com>
2023-08-19 09:40:38 +0330

    create some serializers structure in api/serializer

---
+++ api/serializers/membership.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+
+class WorkspaceMembershipSerializer(serializers.ModelSerializer):
+    ...
+
+
+class BoardMembershipSerializer(serializers.ModelSerializer):
+    ...
+
+
+class CardMembershipSerializer(serializers.ModelSerializer):
+    ...
+
--- api/views/accounts.py
+++ api/views/accounts.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from accounts.models import User
-from api.serializers.accounts import UserSerializers
 
 
 class SignUpAPIView(APIView):
