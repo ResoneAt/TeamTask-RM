d69ac2acadc67d5611c392e4120cef2e5e35b691
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-20 10:05:29 +0330

    create messageserializer api/serializers/message.py

--- api/serializers/message.py
+++ api/serializers/message.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from .models import MessageModel
+
 
 
 class MessagesSerializer(serializers.ModelSerializer):
-    ...
+    class Meta:
+        model = MessageModel
+        fields = ['from_user', 'to_user', 'text']
+        
 
