e00defde8e78f2967f426ee91282abcd4b78ed25
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-20 10:16:01 +0330

    create MessagesListAPIView in api/views/message.py

--- api/views/message.py
+++ api/views/message.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
-
+from rest_framework.response import Response
+from .models import MessageModel
+from .serializers import MessageSerializer
 
 class MessagesListAPIView(APIView):
     def get(self, request):
-        ...
+        message = MessageModel.objects.all()
+        serializer = MessageSerializer(message, many=True)
+        return Response(serializer.data)
+
 
 
 class SendMessageAPIView(APIView):
