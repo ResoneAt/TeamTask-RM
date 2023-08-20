d4fd3638b440adad3eaaaf6cc09d11ea4ea20dcb
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-20 11:05:21 +0330

    create method post in messageslistapiview for api/views/message.py

--- api/views/message.py
+++ api/views/message.py
@@ -3,13 +3,21 @@ from rest_framework.response import Response
 from .models import MessageModel
 from .serializers import MessageSerializer
 
+
 class MessagesListAPIView(APIView):
+
     def get(self, request):
         message = MessageModel.objects.all()
         serializer = MessageSerializer(message, many=True)
         return Response(serializer.data)
-
-
+    
+    def post(self, request):
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
 
 class SendMessageAPIView(APIView):
     def get(self):
