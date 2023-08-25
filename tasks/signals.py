6c39646d3e7d34460983e3453354101e9cef3652
Ramin <eslamiramin85@gmail.com>
2023-08-25 23:52:42 +0330

    create some signals about send email to user for her memberships

--- tasks/signals.py
+++ tasks/signals.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from accounts.models import NotificationModel
@@ -10,9 +11,20 @@ from .models import WSMembershipModel, BMembershipModel, CMembershipModel
 def add_member_to_workspace_signal(sender, request, instance, **kwargs):
     NotificationModel.objects.create(
         body=f'You have been added to {instance.workspace.title} Workspace'
-             f' by {instance.workspace.owner}',
+             f' by {instance.from_user.username}',
         to_user=instance.user
     )
+
+
+@receiver(post_save, sender=WSMembershipModel)
+def send_email_about_add_member_to_workspace_signal(sender, instance, created, **kwargs):
+    if created:
+        subject = 'TeamTask - You have been added to ...'
+        message = (f'You have been added to {instance.workspace.title} Workspace'
+                   f' by {instance.from_user.username}'),
+        from_email = 'TeamTask.group@gmail.com'
+        to_email = instance.to_user.email
+        send_mail(subject, message, from_email, [to_email])
 
 
 @receiver(signal=post_save, sender=BMembershipModel)
@@ -24,9 +36,31 @@ def add_member_to_board_signal(sender, request, instance, **kwargs):
     )
 
 
+@receiver(post_save, sender=BMembershipModel)
+def send_email_about_add_member_to_board_signal(sender, instance, created, **kwargs):
+    if created:
+        subject = 'TeamTask - You have been added to ...'
+        message = (f'You have been added to {instance.board.title} board'
+                   f' by {instance.from_user.username}'),
+        from_email = 'TeamTask.group@gmail.com'
+        to_email = instance.to_user.email
+        send_mail(subject, message, from_email, [to_email])
+
+
 @receiver(signal=post_save, sender=CMembershipModel)
 def assigning_a_card_to_a_member_signal(sender, request, instance, **kwargs):
     NotificationModel.objects.create(
         body=f'A new task has been assigned to you by Ramin as below',
         to_user=instance.user
     )
+
+
+@receiver(post_save, sender=CMembershipModel)
+def send_email_about_add_member_to_card_signal(sender, instance, created, **kwargs):
+    if created:
+        subject = 'TeamTask - You have been added to ...'
+        message = (f'You have been added to {instance.card.title} card'
+                   f' by {instance.from_user.username}'),
+        from_email = 'TeamTask.group@gmail.com'
+        to_email = instance.to_user.email
+        send_mail(subject, message, from_email, [to_email])
