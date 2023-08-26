661f6c2b80cbe1dcda3469fdf810d380d198d7d9
Mahsa R <mahsarm99@gmail.com>
2023-08-26 15:07:12 +0330

    cleaned api/views/card.py  based on flake8

--- api/views/card.py
+++ api/views/card.py
@@ -2,28 +2,32 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from tasks.models import ListModel,BoardModel, CardModel,LabelModel,SubTaskModel
-from api.serializers.card import ListSerializer,LabelSerializer,SubCardSerializer,\
-                                 CardSerializer
+from tasks.models import ListModel, BoardModel, CardModel, LabelModel, SubTaskModel
+from api.serializers.card import ListSerializer, LabelSerializer, SubCardSerializer, CardSerializer
 from accounts.models import User
 from rest_framework import status
+
 
 class MyCardsAPIView(APIView):
     def get(self, request, user_id):
         user = User.objects.get(id=user_id)
         cards = CardModel.objects.filter(cmembership__user=user)
         serializer = CardSerializer(cards, many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-class CardsAPIView(APIView):    
-    def get(self, request,board_id):
+
+class CardsAPIView(APIView):
+    def get(self, request, board_id):
         query_set = CardModel.objects.filter(list__board_id=board_id)
         srz_data = CardSerializer(instance=query_set, many=True)
         return Response(data=srz_data.data, status=status.HTTP_200_OK)
-      
+
+
 class CardAPIView(APIView):
     def get(self, request, card_id):
         card = get_object_or_404(CardModel, id=card_id)
+        srz_data = CardSerializer(instance=card)
+        return Response(data=srz_data.data, status=status.HTTP_200_OK)
 
 
 class CardCreateAPIView(APIView):
@@ -36,14 +40,16 @@ class CardCreateAPIView(APIView):
             return Response(srz_data.data, status=status.HTTP_201_CREATED)
         return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class CardUpdateAPIView(APIView):
     def put(self, request, card_id):
         card = get_object_or_404(CardModel, id=card_id)
-        srz_data = CardSerializer(instance=card,data=request.POST, partial=True)
+        srz_data = CardSerializer(instance=card, data=request.POST, partial=True)
         if srz_data.is_valid():
             srz_data.save()
             return Response(srz_data.data, status=status.HTTP_200_OK)
         return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CardDeleteAPIView(APIView):
     def delete(self, request, card_id):
@@ -66,7 +72,7 @@ class SubCardViewSet(ViewSet):
 
     def partial_update(self, request, pk=None):
         subcard = get_object_or_404(self.queryset, pk=pk)
-        srz_data =SubCardSerializer(instance=subcard, data=request.POST, partial=True)
+        srz_data = SubCardSerializer(instance=subcard, data=request.POST, partial=True)
         if srz_data.is_valid():
             srz_data.save()
             return Response(srz_data.data, status=status.HTTP_200_OK)
@@ -80,8 +86,8 @@ class SubCardViewSet(ViewSet):
 
 class ListViewSet(ViewSet):
     queryset = ListModel.objects.all()
-    
-    def create(self, request, board_id): 
+
+    def create(self, request, board_id):
         board_instance = get_object_or_404(BoardModel, id=board_id)
         srz_data = ListSerializer(data=request.data)
         if srz_data.is_valid():
@@ -91,15 +97,15 @@ class ListViewSet(ViewSet):
         return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def partial_update(self, request, pk=None):
-        list = get_object_or_404(self.queryset,pk=pk)
-        srz_data =ListSerializer(instance=list, data=request.POST, partial=True)
+        list = get_object_or_404(self.queryset, pk=pk)
+        srz_data = ListSerializer(instance=list, data=request.POST, partial=True)
         if srz_data.is_valid():
             srz_data.save()
             return Response(srz_data.data, status=status.HTTP_200_OK)
         return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk):
-        list = get_object_or_404(self.queryset,pk=pk)
+        list = get_object_or_404(self.queryset, pk=pk)
         list.delete()
         return Response({'message': 'list deleted'}, status=status.HTTP_200_OK)
 
@@ -118,7 +124,7 @@ class LabelViewSet(ViewSet):
 
     def partial_update(self, request, pk=None):
         label = get_object_or_404(self.queryset, pk=pk)
-        srz_data =LabelSerializer(instance=label, data=request.POST, partial=True)
+        srz_data = LabelSerializer(instance=label, data=request.POST, partial=True)
         if srz_data.is_valid():
             srz_data.save()
             return Response(srz_data.data, status=status.HTTP_200_OK)
