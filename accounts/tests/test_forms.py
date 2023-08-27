df7630c34944918a71846f2eae194df76cf3af22
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-27 23:40:28 +0330

    create test_empty_data in class TestUserRegistrationForm in acco unts/tests/test_forms.py

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -2,3 +2,14 @@ from django.test import TestCase
 from accounts.forms import UserRegistrationForm
 from django.contrib.auth.models import User
 
+
+class TestUserRegistrationForm(TestCase):
+    def test_valid_data(self):
+        form = UserRegistrationForm(data={'username':'moji', 'email':'moji@gmail.com', 'password1':'mojipass', 'password2':'mojipass'})
+        self.assertTrue(form.is_valid())
+
+    def test_empty_data(self):
+        form = UserRegistrationForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors),4)
+    
