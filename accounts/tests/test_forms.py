9c057a5809907da4544ffc0b1a68e8a1b6296445
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-27 23:55:24 +0330

    create test_unmatched_passwords  in class TestUserRegistrationForm in acco unts/tests/test_forms.py

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -16,16 +16,11 @@ class TestUserRegistrationForm(TestCase):
     def test_exist_email(self):
         User.objects.create_user(username='moji', email='moji@gmail.com', password='mojipass')
         form = UserRegistrationForm(data={'username':'nomoji', 'email': 'moji@gmail.com', 'password1':'mojipass', 'password2':'mojipass'})
-        self.assertEqual(len(form.errors),1)
+        self.assertEqual(len(form.errors), 1)
         self.assertTrue(form.has_error('email'))
-        
 
-
-
-
-
-
-
-
-
+    def test_unmatched_passwords(self):
+        form = UserRegistrationForm(data={'username':'moji', 'email':'moji@gmail.com', 'password1':'mojipass', 'password2':'mojipas'})
+        self.assertEqual(len(form.errors), 1)
+        self.assertTrue(form.has_error)
 
