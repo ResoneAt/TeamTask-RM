5db1eb797acfbe6456d6d6d32f9dd8f2b08998f6
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-27 16:54:45 +0330

    create test_CardDeleteAPIView for url

--- api/tests/test_urls.py
+++ api/tests/test_urls.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
-from api.views.card import MyCardsAPIView, CardAPIView, CardCreateAPIView, CardUpdateAPIView
+from api.views.card import MyCardsAPIView, CardAPIView, CardCreateAPIView, CardUpdateAPIView, \
+                        CardDeleteAPIView
 
 
 class TestUrls(TestCase):
@@ -9,7 +10,7 @@ class TestUrls(TestCase):
         self.assertEqual(resolve(url).func.view_class, MyCardsAPIView)
 
     def test_CardAPIView(self):
-        url = reverse('api:card-detail', args=('1'))
+        url = reverse('api:card-detail', args=('1',))
         self.assertEqual(resolve(url).func.view_class, CardAPIView)
 
     def test_CardCreateAPIView(self):
@@ -18,4 +19,9 @@ class TestUrls(TestCase):
 
     def test_CardUpdateAPIView(self):
         url = reverse('api:update-card', args=('1',))
-        self.assertEqual(resolve(url).func.view_class,CardUpdateAPIView)
+        self.assertEqual(resolve(url).func.view_class, CardUpdateAPIView)
+
+    def test_CardDeleteAPIView(self):
+        url = reverse('api:delete-card', args=('1',))
+        self.assertEqual(resolve(url).func.view_class, CardDeleteAPIView)
+        
