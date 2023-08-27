38f9923b002decf2726c831e67d018c8e797cf28
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-27 23:48:01 +0330

    create test_exist_email  in class TestUserRegistrationForm in acco unts/tests/test_forms.py

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -13,3 +13,19 @@ class TestUserRegistrationForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(len(form.errors),4)
     
+    def test_exist_email(self):
+        User.objects.create_user(username='moji', email='moji@gmail.com', password='mojipass')
+        form = UserRegistrationForm(data={'username':'nomoji', 'email': 'moji@gmail.com', 'password1':'mojipass', 'password2':'mojipass'})
+        self.assertEqual(len(form.errors),1)
+        self.assertTrue(form.has_error('email'))
+        
+
+
+
+
+
+
+
+
+
+
