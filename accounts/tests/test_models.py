452f132888ebec73498166ceb4095a7129689370
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-28 01:09:54 +0330

    create setUp in TestUserModel in accounts/tests/test_models.py

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -3,6 +3,8 @@ from accounts.models import User
 from model_bakery import baker
 
 class TestUserModel(TestCase):
+    def setUp(self):
+        self.user = baker.make(User, email='mojtaba@gmail.com')
+
     def test_model_str(self):
-        user = baker.make(User, email='mojtaba@gmail.com')
-        self.assertEqual(str(user), 'mojtaba@gmail.com')
+        self.assertEqual(str(self.user), 'mojtaba@gmail.com')
