e40c0776f449c11634e149f529371fa8295c77c5
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-27 16:20:30 +0330

    create test_CardCreateAPIView for url

--- api/tests/test_urls.py
+++ api/tests/test_urls.py
@@ -1,9 +1,19 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
-from api.views.card import MyCardsAPIView
+from api.views.card import MyCardsAPIView, CardAPIView, CardCreateAPIView
 
 
 class TestUrls(TestCase):
     def test_MyCardsAPIView(self):
         url = reverse('api:my-cards-list', args=('1',))
         self.assertEqual(resolve(url).func.view_class, MyCardsAPIView)
+
+    def test_CardAPIView(self):
+        url = reverse('api:card-detail', args=('1'))
+        self.assertEqual(resolve(url).func.view_class, CardAPIView)
+
+    def test_CardCreateAPIView(self):
+        url = reverse('api:create-card', args=(1,))
+        self.assertEqual(resolve(url).func.view_class, CardCreateAPIView)
+
+        
