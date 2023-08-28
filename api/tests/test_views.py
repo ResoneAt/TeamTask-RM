c974bd6773abbd00c5c852d862339094ee3bdec8
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-28 16:00:52 +0330

    create test_signup_invalid_data in SignUpViewTest in api/tests/tests_views

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -19,3 +19,14 @@ class SignUpAPIViewTest(APITestCase):
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().username, 'testuser')
     
+    def test_signup_invalid_data(self):
+        url = reverse('api:signup')
+        data = {
+            'username': '',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0) 
