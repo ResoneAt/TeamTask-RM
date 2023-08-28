9b744d9cd923554262d233ca228d196624e2bcc6
Mahsa R <mahsarm99@gmail.com>
2023-08-28 11:52:36 +0330

    create custom permission for board  owner

--- permissons.py
+++ permissons.py
@@ -11,3 +11,17 @@ class IsWorkSpaceOwner(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return obj.owner == request.user
+    
+        
+class IsBoardOwner(BasePermission):
+	message = 'permission denied, you are not the owner'
+
+	def has_permission(self, request, view):
+		return request.user.is_authenticated and request.user
+
+	def has_object_permission(self, request, view, obj):
+		if request.method in SAFE_METHODS:
+			return True
+		return obj.owner == request.user
+
+
