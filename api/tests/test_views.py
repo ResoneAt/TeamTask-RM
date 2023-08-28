fa5e75eb4b70bcac53658bae54736d53d9e8a64e
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-08-28 15:58:40 +0330

    create test_signup_valid_data in SignUpViewTest in api/tests/tests_views

---
+++ api/tests/test_views.py
@@ -0,0 +1,21 @@
+from django.urls import reverse , resolve
+from rest_framework import status
+from rest_framework.test import APITestCase
+from accounts.models import User
+from api.views.accounts import SignUpAPIView
+
+class SignUpAPIViewTest(APITestCase):
+
+    def test_signup_valid_data(self):
+        url = reverse('api:signup')
+        data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testuser')
+    
